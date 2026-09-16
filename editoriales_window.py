18bc99628d99ccd8b2172694bbeaa250f0eb0e61
Andruxp04 <136295638+Andruxp04@users.noreply.github.com>
2026-09-15 19:45:50 -0500

    Add files via upload

---
+++ editoriales_window.py
@@ -0,0 +1,18 @@
+##### editoriales_window.py
+
+ 
+
+from catalogo_simple import SimpleCatalogWindow
+
+
+
+ 
+
+class EditorialesWindow(SimpleCatalogWindow):
+
+    def __init__(self, parent, on_change=None):
+
+        super().__init__(parent, "editoriales", "Gestión de Editoriales")
+
+ 
+
